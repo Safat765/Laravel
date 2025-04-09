624c8837947da14a1fa14d28c3afbf0c8c090c4b
Safat765 <88162223+Safat765@users.noreply.github.com>
2025-04-09 19:16:28 +0600

    department migration file

--- app/database/migrations/2025_04_09_094503_create_semesters_table.php
+++ app/database/migrations/2025_04_09_094503_create_semesters_table.php
@@ -13,13 +13,19 @@ class CreateSemestersTable extends Migration {
 	 */
 	public function up()
 	{
-		Schema::create('semesters', function(Blueprint $table)
-		{
-			$table->increments('semester_id');
-			$table->string('name', 30);
-			$table->integer('created_by');
-			$table->timestamps();
-		});
+		if (!Schema::hasTable('semesters')) {
+			Schema::create('semesters', function(Blueprint $table)
+			{
+				$table->increments('semester_id');
+				$table->string('name', 30);
+				$table->integer('created_by')->unsigned();
+				$table->timestamps();
+				$table->foreign('created_by')
+					->references('user_id')->on('users')
+					->onDelete('cascade')
+					->onUpdate('cascade');
+			});
+		}
 	}
 
 	/**
