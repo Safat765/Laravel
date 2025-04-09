c7efac0d9579f53c68d27fe9155c7da3dc92d4c2
Safat765 <88162223+Safat765@users.noreply.github.com>
2025-04-09 16:19:53 +0600

    Department migration table

---
+++ app/database/migrations/2025_04_09_093223_create_departments_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDepartmentsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('departments', function(Blueprint $table)
+		{
+			$table->increments('department_id');
+			$table->string('name', 30);
+			$table->integer('created_by');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('departments');
+	}
+
+}
