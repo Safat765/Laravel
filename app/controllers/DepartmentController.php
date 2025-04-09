2fd9818498b4dd9b4394f64e95030d015b4d1822
Safat765 <88162223+Safat765@users.noreply.github.com>
2025-04-09 19:15:22 +0600

    dep controller

--- app/controllers/DepartmentController.php
+++ app/controllers/DepartmentController.php
@@ -1,39 +1,85 @@
 <?php
-		
+use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Input;
+use Illuminate\Support\Facades\Session;
 class DepartmentController extends BaseController {
 				
 	public function index()
 	{
-		// Show all items
+		$department = new Department();
+		$search = Input::get('search');
+		
+		if ($search != '') {
+			$data = $department->filter($search);
+			
+			$totalUsers = $data['totalUsers'];
+			$users = $data['users'];
+		} else {
+			$data = $department->showAll();
+			$totalUsers = $data['totalUsers'];
+			$users = $data['users'];
+		}
+
+		$data = compact('users', 'totalUsers', 'search');
+
+		return View::make('Department.index')->with($data);
 	}
 				
 	public function create()
 	{
-		// Show create form
+		$pageName = "Create Department";			
+		$url = url('/departments');
+		$data = compact('url', 'pageName');
+		
+		return View::make('Department/create')->with($data);
 	}
 				
 	public function store()
 	{
-		// Handle form submission
+		$validator = Validator::make(Input::all(), [
+			'name' => 'required|min:3'
+		], [
+			'required' => 'The Department field is required.',
+			'min' => 'The Department must be at least :min characters.'
+		]);
+		
+		if ($validator->fails()) {
+			return Redirect::back()
+			->withErrors($validator);
+		}
+		$department = new Department();
+		$name = Input::get('name');
+		$department->createDepartment($name);
+		
+		// if ($department) {
+		// 	Session::flash('success', 'User created successfully');
+		// 	return Redirect::to('user/view');
+		// } else {
+		// 	Session::flash('message', 'Failed to create user');
+		// 	return Redirect::back();
+		// }
 	}
 				
 	public function show($id)
 	{
-		// Show single item
+		echo "Hello show" . $id;
 	}
 				
 	public function edit($id)
 	{
-		// Show edit form
+		echo "Hello edit" . $id;
 	}
 				
 	public function update($id)
 	{
-		// Handle update
+		echo "Hello update" . $id;
 	}
 				
 	public function destroy($id)
 	{
-		// Handle deletion
+		echo "Hello destroy " . $id;
 	}
 }
