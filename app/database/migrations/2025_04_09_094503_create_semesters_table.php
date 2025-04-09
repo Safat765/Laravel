87737d070089451eb39401ea7a2bc648341d57a9
Safat765 <88162223+Safat765@users.noreply.github.com>
2025-04-09 16:20:45 +0600

    semester migration table

---
+++ app/database/migrations/2025_04_09_094503_create_semesters_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSemestersTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('semesters', function(Blueprint $table)
+		{
+			$table->increments('semester_id');
+			$table->string('name', 30);
+			$table->integer('created_by');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('semesters');
+	}
+
+}
