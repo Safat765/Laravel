954e0418066b7d2d133202a15c71d3c4e5907fb9
Safat765 <88162223+Safat765@users.noreply.github.com>
2025-04-09 19:16:57 +0600

    deperatment model

--- app/models/Department.php
+++ app/models/Department.php
@@ -5,6 +5,7 @@ use Illuminate\Auth\UserInterface;
 use Illuminate\Auth\Reminders\RemindableTrait;
 use Illuminate\Auth\Reminders\RemindableInterface;
 use Illuminate\Database\Eloquent\Model as Eloquent;
+use Carbon\Carbon;
 
 class Department extends Eloquent implements UserInterface, RemindableInterface {
 
@@ -23,5 +24,80 @@ class Department extends Eloquent implements UserInterface, RemindableInterface 
      * @var string
      */
     protected $primaryKey = 'department_id';
+	protected $fillable = [
+		'name',
+        'createdBy'
+	];
+
+    public function createDepartment($name)
+	{
+		$department = new Department();
+		
+		$department->name = $name;
+		$department->createdBy = 11;
+		$department->created_at = Carbon::now('Asia/Dhaka')->format('h:i A');
+		$department->updated_at = "";
+		
+		if ($department->save()) {
+			return $department;
+		}
+		
+		return false;
+	}
+
+    public function filter($search)
+	{
+		$departmentCount = Department::where('name', 'LIKE', '%' . $search . '%')
+		->orWhere('email', 'created_by', '%' . $search . '%');
+		
+		$totalDepartment = $departmentCount->count();
+		$department = $departmentCount->paginate(5);
+		
+		$data = compact('department', 'totalDepartment');
+		return $data;
+	}
+
+    public function showAll()
+	{
+		$departmentCount = Department::all();
+		$totalDepartment = $departmentCount->count();
+		$department = Department::paginate(5);		
+		$data = compact('department', 'totalDepartment');
+
+		return $data;
+	}
+
+    public function edit($id)
+	{
+		$department = Department::find($id);
+		return $department;
+	}
+	
+	public function updateDepartment(array $data, $department_id)
+	{		
+		$department = $this->edit($department_id);
+		
+		if (!$department) {
+			return false;
+		}
+		$department->name = $data['name'];
+		$department->updated_at = Carbon::now('Asia/Dhaka')->format('h:i A');;
+		$department->save();
+		
+		return $department;
+	}
+	
+	public function deleteDepartment($id)
+	{
+		$department = $this->edit($id);
+		
+		if (!$department) {
+			return false;
+		}
+		
+		$department->delete();
+		
+		return $department;
+	}
 
 }
