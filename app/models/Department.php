e5aac4c94ed53ce84d996cb699706efe3c08e9e2
Safat765 <88162223+Safat765@users.noreply.github.com>
2025-04-09 16:21:38 +0600

    department model

---
+++ app/models/Department.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Auth\UserTrait;
+use Illuminate\Auth\UserInterface;
+use Illuminate\Auth\Reminders\RemindableTrait;
+use Illuminate\Auth\Reminders\RemindableInterface;
+use Illuminate\Database\Eloquent\Model as Eloquent;
+
+class Department extends Eloquent implements UserInterface, RemindableInterface {
+
+    use UserTrait, RemindableTrait;
+
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'departments';
+
+    /**
+     * The primary key for the model.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'department_id';
+
+}
