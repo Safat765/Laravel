fb36baa722573b36a725a70ccbfe9218dc9faf06
Safat765 <88162223+Safat765@users.noreply.github.com>
2025-04-09 13:22:44 +0600

    main blade page from where all the pages rederict

--- app/views/layout/main.blade.php
+++ app/views/layout/main.blade.php
@@ -2,9 +2,15 @@
     <div class="container">
         <br>
         @if(Session::has('message'))
-            <div class="alert alert-info text-center">
-                {{ Session::get('message') }}
+            <div class="alert alert-warning alert-dismissible fade show text-center" role="alert">
+                <strong>{{ Session::get('message') }}
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
             </div>
+        @elseif (Session::has('success'))
+            <div class="alert alert-success alert-dismissible fade show text-center" role="alert">
+                <strong>{{ Session::get('success') }}
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+          </div>
         @endif
         @yield('main')
     </div>
