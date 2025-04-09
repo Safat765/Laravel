256f5580c91e09df1cca165262a67879e70056aa
Safat765 <88162223+Safat765@users.noreply.github.com>
2025-04-09 16:21:02 +0600

    semester controller

---
+++ app/controllers/SemesterController.php
@@ -0,0 +1,39 @@
+<?php
+		
+class SemesterController extends BaseController {
+				
+	public function index()
+	{
+		// Show all items
+	}
+				
+	public function create()
+	{
+		// Show create form
+	}
+				
+	public function store()
+	{
+		// Handle form submission
+	}
+				
+	public function show($id)
+	{
+		// Show single item
+	}
+				
+	public function edit($id)
+	{
+		// Show edit form
+	}
+				
+	public function update($id)
+	{
+		// Handle update
+	}
+				
+	public function destroy($id)
+	{
+		// Handle deletion
+	}
+}
