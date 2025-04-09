854ce1688a4998ab6bd14ab36d1ac1fe96ca8977
Safat765 <88162223+Safat765@users.noreply.github.com>
2025-04-09 11:46:41 +0600

    layout main page

--- app/views/layout/main.blade.php
+++ app/views/layout/main.blade.php
@@ -1,5 +1,11 @@
 @include('layout.header')
     <div class="container">
+        <br>
+        @if(Session::has('message'))
+            <div class="alert alert-info text-center">
+                {{ Session::get('message') }}
+            </div>
+        @endif
         @yield('main')
     </div>
 @include('layout.footer')
