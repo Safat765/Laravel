65579ebd8b9c81838dd243419f61d4e6b8351338
Safat765 <88162223+Safat765@users.noreply.github.com>
2025-04-09 19:17:56 +0600

    header page

--- app/views/layout/header.blade.php
+++ app/views/layout/header.blade.php
@@ -12,20 +12,31 @@
     <div>
         <nav class="navbar navbar-expand-lg container-fluid py-2 pb-2" style="background-color: rgb(56, 56, 56);">
             <div class="container-fluid">
-                <a class="navbar-brand text-white" href="#"><b>Navbar</b></a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+                <a class="navbar-brand text-white" href="{{url('/')}}"><b>Navbar</b></a>
                 <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <a class="nav-link active text-white" href="{{url('/')}}">Home</a>
+                    <ul class="nav nav-pills">
+                        {{-- <li class="nav-item">
+                            <a class="nav-link text-white" href="{{url('/')}}">Home</a>
+                        </li> --}}
+                        <li class="nav-item dropdown">
+                            <a class="btn btn-dark dropdown-toggle me-2" data-bs-toggle="dropdown" aria-expanded="false">
+                                User
+                            </a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                <li><a class="dropdown-item" href="{{URL::route('userView')}}">View</a></li>
+                                <hr>
+                                <li><a class="dropdown-item" href="{{URL::route('createUser')}}">Create</a></li>
+                            </ul>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link text-white" href="{{URL::route('userView')}}">View Users</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link text-white" href="{{URL::route('createUser')}}">Create User</a>
+                        <li class="nav-item dropdown">
+                            <a class="btn btn-dark dropdown-toggle me-2" data-bs-toggle="dropdown" aria-expanded="false">
+                                Department
+                            </a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                {{-- <li><a class="dropdown-item" href="{{URL::route('userView')}}">View</a></li> --}}
+                                <hr>
+                                <li><a class="dropdown-item" href="{{URL::route('departments.create')}}">Create</a></li>
+                            </ul>
                         </li>
                         {{-- <li class="nav-item">
                             <a class="nav-link active text-white" href="{{url('/destroy-session')}}">Logout</a>
