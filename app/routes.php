cab5e32417ddffb01e872e6eb3b9496d0b1122b2
Safat765 <88162223+Safat765@users.noreply.github.com>
2025-04-10 14:32:17 +0600

    route file

--- app/routes.php
+++ app/routes.php
@@ -18,16 +18,17 @@ use Illuminate\Support\Facades\Session;
 Route::get('/', 'LoginController@index');
 Route::post('/login', 'LoginController@login');
 
-Route::group(['prefix' => 'user'], function() {
-    Route::get('/create', ['as' => 'createUser', 'uses' => 'UserController@create']);
-    Route::post('/create', 'userController@store');
-	Route::get('/view', ['as' => 'userView', 'uses' => 'UserController@index']);
+Route::group(['prefix' => 'users'], function() {
+//     Route::get('/create', ['as' => 'createUser', 'uses' => 'UserController@create']);
+//     Route::post('/create', 'userController@store');
+// 	Route::get('/view', ['as' => 'userView', 'uses' => 'UserController@index']);
 	
-    Route::get('/delete/{id}', ['as' => 'userDelete', 'uses' => 'UserController@destroy']);
-    Route::get('/edit/{id}', ['as' => 'userEdit', 'uses' => 'UserController@edit']);
-    Route::post('/update/{id}', ['as' => 'userUpdate', 'uses' => 'UserController@update']);
+//     Route::get('/delete/{id}', ['as' => 'userDelete', 'uses' => 'UserController@destroy']);
+//     Route::get('/edit/{id}', ['as' => 'userEdit', 'uses' => 'UserController@edit']);
+//     Route::post('/update/{id}', ['as' => 'userUpdate', 'uses' => 'UserController@update']);
     Route::get('/status/{id}', ['as' => 'userStatus', 'uses' => 'UserController@status']);
 });
 
+Route::resource('/users', 'UserController');
 Route::resource('/departments', 'DepartmentController');
 Route::resource('/semesters', 'SemesterController');
