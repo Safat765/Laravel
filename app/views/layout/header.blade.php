b5398e86368311b6fbb011972b4f61269244c272
Safat765 <88162223+Safat765@users.noreply.github.com>
2025-04-10 17:44:26 +0600

    header for all blade.php file

--- app/views/layout/header.blade.php
+++ app/views/layout/header.blade.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     @stack('title')
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel= "stylesheet" href= "https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css" >
     <link rel="stylesheet" href="/css/pagination.css">
 </head>
 <body>
